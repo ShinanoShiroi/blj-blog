d568e87c4f47a0013dbf03e6d068ec4e37a0a4e1
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-11-26 15:00:26 +0100

    Update home.php

--- home.php
+++ home.php
@@ -5,7 +5,35 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Document</title>
-  <link rel="stylesheet" href="/blog/styles.css">
+  <style>
+   ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+    position: fixed;
+    top: 0;
+    width: 100%;
+  }
+  
+  li {
+    float: left;
+    border-right:1px solid #bbb;
+  }
+  
+  li:last-child {
+    border-right: none;
+  }
+  
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+  </style>
  </head>
  <body>
   <nav>
