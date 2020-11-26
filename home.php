a9f21e2d497ccd4ee12d4509f712f7e6a33acffa
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-11-26 14:58:36 +0100

    Update home.php

--- home.php
+++ home.php
@@ -1,13 +1,18 @@
-<?php include 'nav.php';?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/blog/styles.css">
-</head>
-<body>
-    
-</body>
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+  <link rel="stylesheet" href="/blog/styles.css">
+ </head>
+ <body>
+  <nav>
+    <ul>
+      <li><a href="/blog/">Home</a></li>
+      <li><a href="/blog/blogs.php">Blog</a></li>
+    </ul> 
+  </nav>
+ </body>
 </html>
