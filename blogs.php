c71263bb0cc87ccf6af28ee7cf871103f211aed7
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-11-26 15:02:25 +0100

    Update blogs.php

--- blogs.php
+++ blogs.php
@@ -2,6 +2,9 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+    <meta http-equiv="X-Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+    <meta http-equiv="X-WebKit-CSP" content="default-src 'self'; script-src 'self'">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
